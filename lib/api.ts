bb0319eabdba1a540132fd4983e973bee71ae9a0
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-10-02 00:09:16 +0300

    06-notehub-nextjs-fix_bag

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note, NoteTag } from "../types/";
+import type { Note, NoteTag } from "../types/note";
 
 const API_URL = process.env.NEXT_PUBLIC_NOTEHUB_API_URL;
 const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
